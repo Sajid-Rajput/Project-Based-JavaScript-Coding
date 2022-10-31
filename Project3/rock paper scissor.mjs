641d2d27866a8987a676e0f0503416cafa4535ff
Sajid <sajidrajput01.and@gmail.com>
2022-10-31 16:37:57 +0500

    second commit

--- Project3/rock paper scissor.mjs
+++ Project3/rock paper scissor.mjs
@@ -1,5 +1,45 @@
-import prompSync from "prompt-sync";
-const prompt = prompSync();
+import promptSync from "prompt-sync";
+import randomInteger from "random-int";
 
-const arr = [1,2,11,43,3,5,4];
+// Setup the prompt
+const prompt = promptSync();
+console.log("<-- Ready to play RPS Game -->");
 
+// The available choices
+let choices = ["rock", "paper", "scissor"];
+
+// Computer choose randomly
+let computerChoice = randomInteger(0, 2);
+computerChoice = choices[computerChoice];
+
+// Prompt the question to get the user choice
+let userChoice = +prompt("0 for rock, 1 for paper, or 2 for scissor: ");
+if (userChoice > 2) {
+  console.log("Invalid Input!");
+  throw "Invalid Input!";
+}
+userChoice = choices[userChoice];
+
+// Display the choices
+console.log(`\nComputer Choice: ${computerChoice}`);
+console.log(`Player Choice: ${userChoice}\n`);
+
+// Declare the winner
+let outcome = undefined;
+if (computerChoice === userChoice) {
+  outcome = "Draw";
+} else if (computerChoice === "rock" && userChoice === "paper") {
+  outcome = "Player Wins";
+} else if (computerChoice === "rock" && userChoice === "scissor") {
+  outcome = "Computer Wins";
+} else if (computerChoice === "paper" && userChoice === "rock") {
+  outcome = "Computer Wins";
+} else if (computerChoice === "paper" && userChoice === "scissor") {
+  outcome = "Player Wins";
+} else if (computerChoice === "scissor" && userChoice === "rock") {
+  outcome = "Player Wins";
+} else if (computerChoice === "scissor" && userChoice === "paper") {
+  outcome = "Computer Wins";
+}
+
+console.log(`Result: ${outcome}`);
